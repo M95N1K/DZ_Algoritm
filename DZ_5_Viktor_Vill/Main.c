1625976c5c7cf23c5ad3a264e15a3a502a046eae
M95N1K <mysnik999@gmail.com>
2020-07-07 13:09:45 +0600

    Задание 5 готово

---
+++ DZ_5_Viktor_Vill/Main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#pragma warning(disable : 4996)
+
+// Задание №5
+//	С клавиатуры вводится номер месяца. Требуется определить, к какому времени года он относится.
+// Выполнил Виль В. В.
+
+int main(void)
+{
+	int answ = 0;
+	setlocale(LC_ALL, "RU");
+	setlocale(LC_NUMERIC, "C");
+	printf("Введите номер месяца\n");
+
+	while ((scanf("%d",&answ) != 1) || !(0 < answ && answ < 13))
+	{
+		system("cls");
+		printf("Введите номер месяца (От 1 До 12)\n");
+		while (getchar() != '\n');
+	}
+
+	switch (answ)
+	{
+	case 1:
+	case 2:
+		printf("Холодная Зима");
+		break;
+	case 3:
+	case 4:
+	case 5:
+		printf("Зеленая Весна");
+		break;
+	case 6:
+	case 7:
+	case 8:
+		printf("Жаркое Лето");
+		break;
+	case 9:
+	case 10:
+	case 11:
+		printf("Желтая Осень");
+		break;
+	case 12:
+		printf("Холодная Зима");
+		break;
+	default:
+		break;
+	}
+
+	getch();
+	return 0;
+}
