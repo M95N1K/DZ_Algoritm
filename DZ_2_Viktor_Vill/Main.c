4db66c8a864e0d1222fc77a2d439e58084f2f62e
M95N1K <mysnik999@gmail.com>
2020-07-07 11:17:25 +0600

    2-е домашнее задание готово

---
+++ DZ_2_Viktor_Vill/Main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <locale.h>
+#pragma warning(disable : 4996)
+
+// Задание №2
+//	Найти максимальное из четырех чисел. Массивы не использовать.
+// Выполнил Виль В. В.
+
+int main(void)
+{
+	int a, b, c, d;
+	int count;
+
+	setlocale(LC_ALL, "RU");
+	setlocale(LC_NUMERIC, "C");
+	printf("Введите четрые целых числа:\n");
+
+	count = 1;
+
+	while (count <= 4)
+	{
+		printf("%d %s: ", count, "число");
+		switch (count)
+		{
+		case 1:
+			scanf("%d", &a);
+			break;
+		case 2:
+			scanf("%d", &b);
+			break;
+		case 3:
+			scanf("%d", &c);
+			break;
+		case 4:
+			scanf("%d", &d);
+			break;
+		default:
+			break;
+		}
+		count++;
+	}
+
+	if (a < b)
+		a = b;
+	if (c < d)
+		c = d;
+	if (a < c)
+		a = c;
+
+	printf("%s %d", "Максимальное число:", a);
+	getch();
+	return 0;
+}
