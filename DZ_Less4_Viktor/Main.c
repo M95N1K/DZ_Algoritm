cf53378d46a253d93604067868e2638306f51ee4
M95N1K <mysnik999@gmail.com>
2020-07-17 12:50:39 +0600

    Задание 1 готово

--- DZ_Less4_Viktor/Main.c
+++ DZ_Less4_Viktor/Main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <locale.h>
+#include "Task1.h"
 
 
 #pragma warning(disable : 4996)
@@ -14,7 +15,7 @@ int main()
 	printf("\t2. Реализовать шейкерную сортировку. \n");
 	printf("\t3. Реализовать бинарный алгоритм поиска. \n");
 	printf("\t4. Подсчитать количество операций для каждой из сортировок \n\t\tи сравнить его с асимптотической сложностью алгоритма. \n");
-	while ((scanf("%d", &num) != 1) || !(num == 1 || num == 2 || num == 3 || num == 4))
+	while ((scanf("%d", &num) != 1) || !(num == 1 || num == 2 || num == 3))
 	{
 		printf("Неверный ввод");
 		while (getchar() != '\n');
@@ -26,15 +27,12 @@ int main()
 		task1();
 		break;
 	case 2:
-		task2();
+		//task2();
 		break;
 	case 3:
-		task3();
+		//task3();
 		break;
-	case 4:
-		task4();
-		break;
-
+	
 	default:
 		break;
 	}
