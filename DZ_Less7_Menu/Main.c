718195773a50e6811fc8343e3d4c82f361b113cc
M95N1K <mysnik999@gmail.com>
2020-07-28 10:08:33 +0600

    Меню создано

---
+++ DZ_Less7_Menu/Main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <locale.h>
+//#include "Task1.h"
+
+
+#pragma warning(disable : 4996)
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	setlocale(LC_NUMERIC, "C");
+	int num;
+	printf("Выберите задачу для решения: \n");
+	printf("\t1. Реализовать перевод из десятичной в двоичную систему счисления с использованием стека. \n");
+	printf("\t2. Написать программу, которая определяет, является ли введенная скобочная последовательность правильной. \n");
+	printf("\t3. Реализовать очередь. \n");
+	printf("\t4. Создать функцию, копирующую односвязный список. \n");
+	while ((scanf("%d", &num) != 1) || !(num == 1 || num == 2 || num == 3 || num == 4))
+	{
+		printf("Неверный ввод");
+		while (getchar() != '\n');
+	}
+
+	switch (num)
+	{
+	case 1:
+		//task1();
+		break;
+	case 2:
+		//task2();
+		break;
+	case 3:
+		//task3();
+		break;
+	case 4:
+		//task4();
+		break;
+
+	default:
+		break;
+	}
+
+	getch();
+	return 0;
+}
